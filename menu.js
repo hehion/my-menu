d4bcd875d65dc8c676b8b23cd73b4735642c1d1a
hehion <84363555+hehion@users.noreply.github.com>
2026-01-24 18:37:03 +0900

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,20 @@
+document.write(`
+<div align="left" style="font-size:2.5vmin;">
+  <a href="https://hehion.blogspot.com/p/flytothe.html"><b>가라!종이비행기</b></a>
+  　<a href="https://hehion.blogspot.com/p/i.html"><b>hh</b></a>
+  　<a href="https://hhtoday.blogspot.kr/"><font color="fafafa">'s today</font></a>
+  　<a href="https://hhyesterday.blogspot.kr/p/20130728.html"><font color="fafafa">'s yesterday</font></a>
+  　<a href="https://hhfavorite.blogspot.kr/"><font color="fafafa">'s ☆</font></a>
+</div>
+<div align="right" style="font-size:2.5vmin;">
+  　<a href="https://hehion.blogspot.kr/">home</a>
+  　<a href="https://hhscrap.blogspot.kr/">scrap</a>
+  　<a href="https://hharchug.blogspot.kr/">ARCH</a>
+  <a href="https://hhinnc.blogspot.kr"><font style="font-size:1.8vmin;">..in</font>NC</a>
+  <a href="https://hhincs.blogspot.com/2018/06/20185.html"><font style="font-size:1.8vmin;">..in</font>CS</a>
+  <a href="https://hharchstudy.blogspot.com"><font style="font-size:1.8vmin;">arch</font>STUDY</a>
+  　<a href="https://hhchef.blogspot.com">Chef!</a>
+  　<a href="https://hhlearnrun.blogspot.com">Learn!Run!</a>
+</div>
+<br /><hr />
+`);
