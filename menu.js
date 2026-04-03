d9ac1c0494ce42b842942f321bcb090fe6a6ee95
hehion <84363555+hehion@users.noreply.github.com>
2026-04-03 19:02:23 +0900

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,32 +1,37 @@
 (function() {
   const menuHTML = `
-<div align="left" style="font-size:2.5vmin;">
-  <a href="https://hehion.blogspot.com/p/flytothe.html"><b>가라!종이비행기</b></a>
-  　<a href="https://hehion.blogspot.com/p/i.html"><b>hh</b></a>
-  　<a href="https://hhscrap.blogspot.kr/">scrap</a>
-  　<a href="https://hehion.blogspot.kr/"><b>home</b></a>
-  　<a href="https://hhtoday.blogspot.kr/"><font color="fafafa">'s today</font></a>
-  　<a href="https://hhyesterday.blogspot.kr/p/20130728.html"><font color="fafafa">'s yesterday</font></a>
-  　<a href="https://hhtomorrow.blogspot.com"><font color="fafafa">'s tomorrow</font></a>
-  　<a href="https://hhfavorite.blogspot.kr/"><font color="fafafa">'s ☆</font></a>
+<div align="left" style="font-size: 2.5vmin;">
+<a href="https://hehion.blogspot.com/p/flytothe.html"><b>가라!종이비행기</b></a>
+　<a href="https://hehion.blogspot.com/p/i.html"><b>hh</b></a>
+　<a href="https://hhtoday.blogspot.kr/" style="font-size: 2.5vmin;"><font color="fafafa">'s today</font></a>
+　<a href="https://hhyesterday.blogspot.kr/p/20130728.html" style="font-size: 2.5vmin;"><font color="fafafa">'s yesterday</font></a>
+　<a href="https://hhtomorrow.blogspot.com" style="font-size: 2.5vmin;"><font color="fafafa">'s tomorrow</font></a>
+　<a href="https://hhfavorite.blogspot.kr/" style="font-size: 2.5vmin;"><font color="fafafa">'s ☆</font></a>
 </div>
-<div align="right" style="font-size:2.5vmin;">
-<a href="https://hharchzitda.blogspot.com"><font style="font-size:1.8vmin;">arch</font>ZITDA</a>
-  <a href="https://hharchlesson.blogspot.com"><font style="font-size:1.8vmin;">arch</font>Lesson</a>
-  <a href="https://hharchstudy.blogspot.com"><font style="font-size:1.8vmin;">arch</font>STUDY</a>
-  <a href="https://hharchug.blogspot.kr/">ARCH<font style="font-size:1.8vmin;">ug</font></a>
-  <a href="https://hhinnc.blogspot.kr"><font style="font-size:1.8vmin;">..in</font>NC</a>
-  <a href="https://hhincs.blogspot.com/2018/06/20185.html"><font style="font-size:1.8vmin;">..in</font>CS</a><br>
-  　<a href="https://hhlearnrun.blogspot.com">Learn!Run!</a>
-  　<a href="https://hhchef.blogspot.com">Chef!</a>
-  　<a href="https://hh-movement.blogspot.com">Move<font style="font-size:1.8vmin;">ment</font></a>
-  　<a href="https://hhlanguage.blogspot.com">L<font style="font-size:1.8vmin;">anguage</font></a>
+<div style="font-size: 2.5vmin;">
+<table style="width: 100%;"><tbody><tr><td align="left" style="font-size: 2.5vmin;" width="30%">
+<a href="https://hehion.blogspot.kr/"><b>home</b></a>
+　<a href="https://hhscrap.blogspot.kr/" style="font-size: 2.5vmin; text-align: left;">scrap</a>
+</td><td align="right" style="font-size: 2.5vmin;" width="70%">
+<a href="https://hharchzitda.blogspot.com"><font style="font-size: 1.8vmin;">arch</font>ZITDA</a>
+ <a href="https://hharchlesson.blogspot.com"><font style="font-size: 1.8vmin;">arch</font>Lesson</a>
+ <a href="https://hharchstudy.blogspot.com"><font style="font-size: 1.8vmin;">arch</font>STUDY</a>
+ <a href="https://hharchug.blogspot.kr/">ARCH<font style="font-size: 1.8vmin;">ug</font></a>
+ <a href="https://hhinnc.blogspot.kr"><font style="font-size: 1.8vmin;">..in</font>NC</a>
+ <a href="https://hhincs.blogspot.com/2018/06/20185.html"><font style="font-size: 1.8vmin;">..in</font>CS</a>
+</td></tr></tbody></table></div>
+<div align="right" style="font-size: 2.5vmin;">
+　<a href="https://hhlearnrun.blogspot.com">Learn!Run!</a>
+　<a href="https://hhchef.blogspot.com">Chef!</a>
+　<a href="https://hh-movement.blogspot.com">Move<font style="font-size: 1.8vmin;">ment</font></a>
+　<a href="https://hhlanguage.blogspot.com">L<font style="font-size: 1.8vmin;">anguage</font></a>
 <br />
-  　<a href="https://hhdrawing.blogspot.com">Draw</a>
-  　<a href="https://hhplaysing.blogspot.com">Play<font style="font-size:1.8vmin;">&</font>Sing</a>
-  　<a href="https://hhcoding.blogspot.com">C<font style="font-size:1.8vmin;">oding</font></a>
-  　<a href="https://hhmathscience.blogspot.com">M<font style="font-size:1.8vmin;">ath</font>S<font style="font-size:1.8vmin;">cience</font></a>
+　<a href="https://hhdrawing.blogspot.com">Draw</a>
+　<a href="https://hhplaysing.blogspot.com">Play<font style="font-size: 1.8vmin;">&amp;</font>Sing</a>
+　<a href="https://hhcoding.blogspot.com">C<font style="font-size: 1.8vmin;">oding</font></a>
+　<a href="https://hhmathscience.blogspot.com">M<font style="font-size: 1.8vmin;">ath</font>S<font style="font-size: 1.8vmin;">cience</font></a>
 <br /><hr />
+</div>
 `;
 
   // 화면에 즉시 뿌려주기
